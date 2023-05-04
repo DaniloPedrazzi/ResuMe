644fea708696c1fd44d72e0a15c7f1da6f51f82b
DaniloPedrazzi <danilo.justino1@gmail.com>
2023-05-03 23:10:48 -0300

    Correção de bugs + Estilização final

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -45,7 +45,7 @@ const Demo = () => {
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
   }
-  
+
   // Variável que armazena o link copiado
   const [copied, setCopied] = useState("");
   
@@ -63,16 +63,16 @@ const Demo = () => {
       <div className="flex flex-col w-full gap-2">
         <form onSubmit={handleSubmit} className="relative flex justify-center items-center">
           <img src={linkIcon} alt="copy link" className="absolute left-0 my-2 ml-3 w-5"/>
-          <input type="url" placeholder="URL do artigo" value="" onChange={(e) => setarticle({... article, url: e.target.value})} required className="url_input peer"/>
+          <input type="url" placeholder="URL do artigo" value={article.url} onChange={(e) => setArticle({... article, url: e.target.value})} required className="url_input peer"/>
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↵</button>
         </form>
           <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-            {allArticles.map((item, index) => (
+            {allArticles.reverse().map((item, index) => (
               <div key={`link-${index}`} onClick={() => setArticle(item)} className="link_card">
                 <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                   <img src={copied === item.url ? tick : copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
-                  <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
                 </div>
+                  <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
               </div>
             ))}
           </div>
@@ -92,10 +92,10 @@ const Demo = () => {
           article.summary && (
             <div className='flex flex-col gap-3'>
               <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
+              <span className='purple_gradient'>Resumo </span> do Artigo
               </h2>
-              <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-700'>
+              <div>
+                <p className='font-inter font-medium text-sm text-gray-400'>
                   {article.summary}
                 </p>
               </div>
