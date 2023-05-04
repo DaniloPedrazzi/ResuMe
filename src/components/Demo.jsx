e91915c794b6fb963824b09a5e3695bbf39cbc9a
DaniloPedrazzi <danilo.justino1@gmail.com>
2023-05-03 22:51:09 -0300

    Sistema de copiar

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -12,13 +12,14 @@ const Demo = () => {
   // Variável com todos os artigos
   const [allArticles, setAllArticles] = useState([]);
 
+  
   //Variável para chamar a API
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
-
+  
   // Carrega todos os arquivos do localStorage para a variável allArticles (inicia-se no primeiro render)
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
-
+    
     if(articlesFromLocalStorage){
       setAllArticles(articlesFromLocalStorage);
     }
@@ -30,7 +31,7 @@ const Demo = () => {
     
     //Chama a API
     const { data } = await getSummary({articleUrl: article.url});
-
+    
     if(data?.summary){
       //Pega o retorno da API e coloca na variável article
       const newArticle = {...article, summary: data.summary};
@@ -39,10 +40,22 @@ const Demo = () => {
       //Pega o novo artigo e coloca na lista de todos os artigos
       const updatedAllArticles = [newArticle, ...allArticles];
       setAllArticles(updatedAllArticles);
-
+      
       //Salva a lista de todos os artigos no localStorage
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
+  }
+  
+  // Variável que armazena o link copiado
+  const [copied, setCopied] = useState("");
+  
+  //Seta a copyUrl (durante 3s) como o link do artigo e copia para a área de transferência
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
   }
 
   return (
@@ -56,8 +69,8 @@ const Demo = () => {
           <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
             {allArticles.map((item, index) => (
               <div key={`link-${index}`} onClick={() => setArticle(item)} className="link_card">
-                <div className="copy_btn">
-                  <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
+                <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                  <img src={copied === item.url ? tick : copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
                   <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
                 </div>
               </div>
