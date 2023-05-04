8dc8634dc1f16678115ecf3a37eaf51bb69ecdd4
DaniloPedrazzi <danilo.justino1@gmail.com>
2023-05-03 22:44:22 -0300

    Salvar resumos + Mostrar resumos + Comentários

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,22 +3,45 @@ import { copy, linkIcon, loader, tick } from "../assets"
 import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
-  const [article, setarticle] = useState({
+  // Variável do artigo atual
+  const [article, setArticle] = useState({
     url: '',
     summary: ''
   });
 
+  // Variável com todos os artigos
+  const [allArticles, setAllArticles] = useState([]);
+
+  //Variável para chamar a API
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  // Carrega todos os arquivos do localStorage para a variável allArticles (inicia-se no primeiro render)
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+
+    if(articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, [])
   
+  //Chama quando aperta o botão principal
   const handleSubmit = async (e) => {
     e.preventDefault();
     
+    //Chama a API
     const { data } = await getSummary({articleUrl: article.url});
 
     if(data?.summary){
+      //Pega o retorno da API e coloca na variável article
       const newArticle = {...article, summary: data.summary};
+      setArticle(newArticle);
+      
+      //Pega o novo artigo e coloca na lista de todos os artigos
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setAllArticles(updatedAllArticles);
 
-      setarticle = newArticle;
+      //Salva a lista de todos os artigos no localStorage
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
   }
 
@@ -30,7 +53,42 @@ const Demo = () => {
           <input type="url" placeholder="URL do artigo" value="" onChange={(e) => setarticle({... article, url: e.target.value})} required className="url_input peer"/>
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↵</button>
         </form>
-
+          <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+            {allArticles.map((item, index) => (
+              <div key={`link-${index}`} onClick={() => setArticle(item)} className="link_card">
+                <div className="copy_btn">
+                  <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
+                  <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+      </div>
+      <div className="my-10 max-w-full flex justify-center items-center">
+      {isFetching ? ( //Se estiver carregando, mostra o loader
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? ( //Se der erro, mostra o erro
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : ( //Se não tiver erro, e tiver resumo, mostra o resumo
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   )
